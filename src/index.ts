f64841c4c812e810b95af1ea24c7c8bfcc08a463
Stanley Huang <huang.stanley050@gmail.com>
2019-10-08 11:26:45 +1100

    getting map on the browser showing

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import { Company } from "./Company";
 
 const user = new User();
 const company = new Company();
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lng: 0, lat: 0 }
+});
